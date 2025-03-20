36c478295a8d82329ef14d90e20a15eec001648f
Harshit Aranya <81676573+HarshitAranya@users.noreply.github.com>
2025-03-20 13:48:12 +0530

    Update AWSControllerVM.sh

--- AWSControllerVM.sh
+++ AWSControllerVM.sh
@@ -1,6 +1,6 @@
 alias terraform=tf
 git config --global user.name "Harshit Aranya"
-git config --global user.email harshit.aranya@gmail.com
+git config --global user.email harshit.aranya@email.com
 git remote add origin https://github.com/HarshitAranya/devops.git
 
 aws ec2 describe-instances --filters "Name=tag:Name,Values=AWSControllerVM" --query "Reservations[].Instances[].PublicIpAddress" #--output text
