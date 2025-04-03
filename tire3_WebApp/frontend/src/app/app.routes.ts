7090d4b52b2df460056d6cfb2ea71070d8e6b580
Harshit Aranya <harshit.aranya@gmail.com>
2025-04-03 17:56:27 +0530

    minikube configured for back, db and frontend

--- tire3_WebApp/frontend/src/app/app.routes.ts
+++ tire3_WebApp/frontend/src/app/app.routes.ts
@@ -32,6 +32,12 @@ export const routes: Routes = [
                 path: 'my-users',
                 component: MyUsersComponent
             },
+            { 
+                path: '',
+                redirectTo: '/my-users',
+                pathMatch: 'full'
+
+            },
             {
                 path: 'post-api',
                 component: PostApiComponent
